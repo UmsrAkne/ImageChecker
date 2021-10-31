9084a24ab9054d4b7b87c7467a3631219418541f
Akn <sibata486@gmail.com>
2021-10-31 19:46:33 +0900

    init

---
+++ ImageChecker/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ImageChecker
+{
+    using System.Windows;
+    using ImageChecker.Views;
+    using Prism.Ioc;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
