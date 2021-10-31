c7a76e1eb7e6e35cff2ef24f3ed84d2da525eb0c
Akn <sibata486@gmail.com>
2021-10-31 20:18:05 +0900

    add / Bitmap を読み込む処理を実装

--- ImageChecker/Models/ImageLoader.cs
+++ ImageChecker/Models/ImageLoader.cs
@@ -7,10 +7,22 @@
 
     public class ImageLoader
     {
+        public List<ImageFile> ImageFilesA { get; private set; } = new List<ImageFile>();
+
+        public List<ImageFile> ImageFilesB { get; private set; } = new List<ImageFile>();
+
+        public List<ImageFile> ImageFilesC { get; private set; } = new List<ImageFile>();
+
+        public List<ImageFile> ImageFilesD { get; private set; } = new List<ImageFile>();
+
         public void Load(string directoryPath)
         {
             List<string> fileNames = Directory.GetFiles(directoryPath, "*.png").Concat(Directory.GetFiles(directoryPath, "*.jpg")).ToList();
-            List<Bitmap> bitmaps = fileNames.Select(fn => new Bitmap(fn)).ToList();
+
+            ImageFilesA = fileNames.Where(name => name.Contains("A")).Select(name => new ImageFile(name)).ToList();
+            ImageFilesB = fileNames.Where(name => name.Contains("B")).Select(name => new ImageFile(name)).ToList();
+            ImageFilesC = fileNames.Where(name => name.Contains("C")).Select(name => new ImageFile(name)).ToList();
+            ImageFilesD = fileNames.Where(name => name.Contains("D")).Select(name => new ImageFile(name)).ToList();
         }
     }
 }
