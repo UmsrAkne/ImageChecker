974fcad433337848946e6532dd76bd7bc10be320
Akn <sibata486@gmail.com>
2021-10-31 20:04:17 +0900

    add / 画像をロードするクラスを追加

---
+++ ImageChecker/Models/ImageLoader.cs
@@ -0,0 +1,16 @@
+﻿namespace ImageChecker.Models
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+
+    public class ImageLoader
+    {
+        public void Load(string directoryPath)
+        {
+            List<string> fileNames = Directory.GetFiles(directoryPath, "*.png").Concat(Directory.GetFiles(directoryPath, "*.jpg")).ToList();
+            List<Bitmap> bitmaps = fileNames.Select(fn => new Bitmap(fn)).ToList();
+        }
+    }
+}
