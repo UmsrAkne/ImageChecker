8029f11f11c1156e75e4e735cd6100647caa73bb
Akn <sibata486@gmail.com>
2021-11-06 23:20:36 +0900

    add / 画像の表示位置をコントロールできるよう実装

--- ImageChecker/Models/ImageFile.cs
+++ ImageChecker/Models/ImageFile.cs
@@ -1,14 +1,22 @@
 ﻿namespace ImageChecker.Models
 {
-    using System.Drawing;
     using System.IO;
+    using Prism.Mvvm;
 
-    public class ImageFile
+    public class ImageFile : BindableBase
     {
+
+        private int x = 0;
+        private int y = 0;
+
         public ImageFile(string filePath)
         {
             FileInfo = new FileInfo(filePath);
         }
+
+        public int X { get => x; set => SetProperty(ref x, value); }
+
+        public int Y { get => y; set => SetProperty(ref y, value); }
 
         public FileInfo FileInfo { get; private set; }
 
