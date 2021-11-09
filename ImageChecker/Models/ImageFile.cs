c0c812cbe81251bd596951e772205a480e5fbc0b
Akn <sibata486@gmail.com>
2021-11-09 22:15:11 +0900

    add / ImageFIle に、画像の幅、高さのプロパティを追加しビューとバインド
    画像のサイズが常に正しく表示されるようにするための変更

--- ImageChecker/Models/ImageFile.cs
+++ ImageChecker/Models/ImageFile.cs
@@ -1,5 +1,6 @@
 ﻿namespace ImageChecker.Models
 {
+    using System.Drawing;
     using System.IO;
     using Prism.Mvvm;
 
@@ -7,15 +8,28 @@
     {
         private int x = 0;
         private int y = 0;
+        private int width;
+        private int height;
+        private Bitmap bitmap;
 
         public ImageFile(string filePath)
         {
             FileInfo = new FileInfo(filePath);
+            if (File.Exists(filePath))
+            {
+                bitmap = new Bitmap(filePath);
+                Width = bitmap.Width;
+                Height = bitmap.Height;
+            }
         }
 
         public int X { get => x; set => SetProperty(ref x, value); }
 
         public int Y { get => y; set => SetProperty(ref y, value); }
+
+        public int Width { get => width; set => SetProperty(ref width, value); }
+
+        public int Height { get => height; set => SetProperty(ref height, value); }
 
         public FileInfo FileInfo { get; private set; }
 
