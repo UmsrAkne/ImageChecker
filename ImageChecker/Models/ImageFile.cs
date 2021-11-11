89a2818b6bacc3d699efffcc106b0ca2f5114eca
Akn <sibata486@gmail.com>
2021-11-11 23:25:19 +0900

    add / ImageFile をグループ分けするための Index プロパティを実装

--- ImageChecker/Models/ImageFile.cs
+++ ImageChecker/Models/ImageFile.cs
@@ -2,6 +2,7 @@
 {
     using System.Drawing;
     using System.IO;
+    using System.Text.RegularExpressions;
     using Prism.Mvvm;
 
     public class ImageFile : BindableBase
@@ -20,7 +21,14 @@
                 bitmap = new Bitmap(filePath);
                 Width = bitmap.Width;
                 Height = bitmap.Height;
-                System.Diagnostics.Debug.WriteLine($"{filePath},{Width},{Height}");
+            }
+
+            if (Regex.Match(FileInfo.Name, @"[ABCD]\d\d\d\d").Success)
+            {
+                IsMatchingNamingRule = true;
+                MatchCollection matches = Regex.Matches(FileInfo.Name, @"[ABCD](\d\d)(\d\d)");
+                Index = int.Parse(matches[0].Groups[1].Value);
+                SubIndex = int.Parse(matches[0].Groups[2].Value);
             }
         }
 
@@ -31,6 +39,12 @@
         public int Width { get => width; set => SetProperty(ref width, value); }
 
         public int Height { get => height; set => SetProperty(ref height, value); }
+
+        public bool IsMatchingNamingRule { get; private set; }
+
+        public int Index { get; private set; }
+
+        public int SubIndex { get; private set; }
 
         public FileInfo FileInfo { get; private set; }
 
