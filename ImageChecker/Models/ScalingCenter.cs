4d293928dd1ea7a3a7ce3c994680e4024cd4e944
Akn <sibata486@gmail.com>
2023-03-16 01:14:30 +0900

    add enum / 画像の中心点を表すための enum を追加

---
+++ ImageChecker/Models/ScalingCenter.cs
@@ -0,0 +1,15 @@
+namespace ImageChecker.Models
+{
+    public enum ScalingCenter
+    {
+        /// <summary>
+        /// 拡大の中心点が中心であることを表します。
+        /// </summary>
+        Center,
+
+        /// <summary>
+        /// 拡大の中心点が左上であることを表します。
+        /// </summary>
+        TopLeft,
+    }
+}
